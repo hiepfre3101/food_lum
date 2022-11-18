0e4374eacc5ff3dd7e07b70e5d642dbf4e0278a8
screaminGreen <101007489+hiepfre3101@users.noreply.github.com>
2022-11-18 11:42:22 +0700

    Cau hinh file index, them render-ctr

---
+++ view/footer.php
@@ -0,0 +1,77 @@
+<!--body main content-->
+<footer class="text-light">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3 col-lg-4 col-xl-3">
+        <h5>DANH MỤC MÓN ĂN</h5>
+        <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
+        <ul class="list-unstyled">
+          <li><a href="">Ưu Đãi</a></li>
+          <li><a href="">Món Mới</a></li>
+          <li><a href="">Combo 1 Người</a></li>
+          <li><a href="">Combo Nhóm</a></li>
+          <li><a href="">Gà Rán - Gà Quay</a></li>
+        </ul>
+      </div>
+
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto">
+        <h5>VỀ KFC</h5>
+        <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
+        <ul class="list-unstyled">
+          <li><a href="">Câu Chuyện Của Chúng Tôi</a></li>
+          <li><a href="">Tin Khuyến Mãi</a></li>
+          <li><a href="">Tin tức KFC</a></li>
+          <li><a href="">Tuyển dụng</a></li>
+          <li><a href="">Đặt tiệc Sinh nhật</a></li>
+        </ul>
+      </div>
+
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto">
+        <h5>LIÊN HỆ KFC</h5>
+        <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
+        <ul class="list-unstyled">
+          <li><a href="">Theo dõi đơn hàng</a></li>
+          <li><a href="">Hệ Thống Nhà Hàng</a></li>
+          <li><a href="">Liên hệ KFC</a></li>
+        </ul>
+      </div>
+
+      <div class="col-md-3 col-lg-3 col-xl-3">
+        <h5>CHÍNH SÁCH</h5>
+        <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
+        <ul class="list-unstyled">
+          <li><a href="">Chính sách hoạt động</a></li>
+          <li><a href="">Chính sách và quy định</a></li>
+          <li><a href="">Chính sách bảo mật thông tin</a></li>
+        </ul>
+      </div>
+
+    </div>
+    <p class="name">Copyright © 2022 KFC Vietnam</p>
+    <hr color="white">
+    <br>
+    <div class="row">
+      <div class="col-lg-6">
+        <p class="congty">CÔNG TY LIÊN DOANH TNHH KFC VIỆT NAM</p>
+        <p class="n">Số 292 Bà Triệu, P. Lê Đại Hành, Q. Hai Bà Trưng, TP. Hà Nội. <br>
+          Điện thoại: (028) 38489828 <br>
+          Mã số thuế: 0100773885 <br>
+          Ngày cấp: 29/10/1998 - Nơi cấp: Cục Thuế Thành Phố Hà Nộ
+        </p>
+      </div>
+      <div class="col-lg-6">
+        <img class="cap" src="https://kfcvn-static.cognizantorderserv.com/images/email/logo_footer.png" alt="">
+      </div>
+    </div>
+    <div class="col-12 copyright mt-3">
+      <p class="float-left">
+        <a href="#">Về đầu trang</a>
+      </p>
+    </div>
+  </div>
+</footer>
+<!--Optional JavaScript-->
+<!--jQuery first, then Popper.js, then Bootstrap JS-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+</html>
