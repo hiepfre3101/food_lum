093a9ba291e1fd314ecaf991ba76b1931fc18933
screaminGreen <101007489+hiepfre3101@users.noreply.github.com>
2022-11-23 10:59:48 +0700

    Hover tooltip header, validate signup, add new product

--- admin/view/form-add-product.php
+++ admin/view/form-add-product.php
@@ -3,14 +3,20 @@
 
 <div class="form-add-product">
     <p class="title">Thêm mới sản phẩm</p>
-    <form action="">
+    <form action="" class="form-add">
         <div class="row-form">
             <div class="row-left"><label for="name-form">Tên sản phẩm : </label></div>
-            <div class="row-rigth"><input type="text" name="name-product-add" id="name-form"></div>
+            <div class="row-rigth">
+                <input type="text" name="name-product-add" id="name-form">
+                <p class="form-message" style="color:red;"></p>
+            </div>
         </div>
         <div class="row-form">
             <div class="row-left"><label for="price-form">Giá sản phẩm : </label></div>
-            <div class="row-rigth"><input type="number" name="price-product-add" id="price-form"></div>
+            <div class="row-rigth">
+                <input type="number" name="price-product-add" id="price-form">
+                <p class="form-message" style="color:red;"></p>
+            </div>
         </div>
         <div class="row-form">
             <div class="row-left"><label for="category-form">Loại sản phẩm : </label></div>
@@ -21,22 +27,40 @@
                         <option value="<?=$value["iddm"]?>"><?=$value["categories_name"]?></option>
                     <?php endforeach?>
                 </select>
+                <p class="form-message" style="color:red;"></p>
             </div>
         </div>
         <div class="row-form">
             <div class="row-left"><label for="img-form">Ảnh sản phẩm : </label></div>
             <div class="row-rigth">
                 <label for="img-form" class="wrapper-img">
-                    <img src="../../public/img/slider1.jpg" alt="">
                 </label>
-                <input type="file" name="img-product-add" id="img-form" hidden>
+                <input type="file" name="img-product-add" id="img-form" >
+                <p class="form-message" style="color:red;"></p>
             </div>
         </div>
         <div class="row-form">
             <div class="row-left"><label for="description-form">Mô tả sản phẩm : </label></div>
-            <div class="row-rigth"><textarea name="description" id="description-form" cols="30" rows="10"></textarea></div>
+            <div class="row-rigth">
+                <textarea name="description" id="description-form" cols="30" rows="10"></textarea>
+                <p class="form-message" style="color:red;"></p>
+            </div>
         </div>
         <button>Thêm sản phảm</button>
     </form>
 </div>
+<script src="./public/js/validate.js"></script>
+<script>
+    validator({
+       form:".form-add",
+       erroSelector:".form-message",
+       rules:[
+           validator.isRequired("#name-form"),
+           validator.isRequired("#price-form"),
+           validator.isRequired("#category-form"),
+           validator.isRequired("#img-form"),
+           validator.isRequired("#description-form"),
+       ]
+    });
+</script>
 <?php require_once("./admin/view/footer.php")?>
