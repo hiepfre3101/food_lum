4241f6ea1c96d504b31a71140c427cd582f93799
Duyy2707 <117347752+Duyy2707@users.noreply.github.com>
2022-12-03 15:27:01 +0700

    xoa bl

--- controller/c_comment.php
+++ controller/c_comment.php
@@ -41,5 +41,25 @@ function showCmtByProduct($id){
     }
     render("product-detail", ["cmt" => $cmt, "product" => $product], 0);
 }
-
+// delete comment
+function deleteAllComment()
+{
+    if (isset($_GET['idDelete'])) {
+        deleteComment($_GET['idDelete']);
+      
+    } else {
+        $cmt =getAllDataComment();
+        
+        foreach ($_POST as $key => $value) {
+            if ($value == "on") {
+                foreach ($cmt as $valuePro) {
+                    if ($valuePro['idcm'] = $key) {
+                        deleteComment($valuePro['idcm']);
+                    }
+                }
+            }
+        }
+    }
+    header("location:index.php?ctr=comment-list");
+}
 ?>
